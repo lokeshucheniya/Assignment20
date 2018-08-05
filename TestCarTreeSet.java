3cfbbdf8f6a11ea710e1bb40f15ca3a5348d8d8c
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 22:28:01 +0530

    Update TestCarTreeSet.java

--- TestCarTreeSet.java
+++ TestCarTreeSet.java
@@ -7,12 +7,15 @@ import in.co.capgemini.Car;
 public class TestCarTreeSet {
 
 	public static void main(String args[]) {
+		
+		//Creating TreeSet for Class Car
 		TreeSet<Car> treeset = new TreeSet<>();
 
 		treeset.add(new Car("Diablo", 600000, 1998, "Chrevolet"));
 		treeset.add(new Car("Focus", 300000, 1990, "Fords"));
 		treeset.add(new Car("Indica", 200000, 1890, "Tata"));
 
+		//Lambda expression for printing value
 		treeset.stream().forEach((name) -> System.out.println(name));
 
 	}
