509384dc3c60947e87a03081f37e1e57b382cd1b
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 22:26:48 +0530

    Update Television.java

--- Television.java
+++ Television.java
@@ -1,10 +1,14 @@
 package in.co.capgemini;
 
 public class Television {
+	
+	//Instance Variable
 	private String company;
 	private String type;
 	private String enabled;
 	private int price;
+	
+	//Parameterised Constructor
 	public Television(String company, String type, String enabled, int price) {
 		super();
 		this.company = company;
@@ -12,6 +16,8 @@ public class Television {
 		this.enabled = enabled;
 		this.price = price;
 	}
+	
+	//Getters and Setters
 	public String getCompany() {
 		return company;
 	}
@@ -36,10 +42,10 @@ public class Television {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
 	@Override
 	public String toString() {
 		return "Television [company=" + getCompany() + ", type=" + getType() + ", enabled=" + getEnabled() + ", price=" + getPrice() + "]";
 	}
-	
 	
 }
